f72e40bc29ea21953220803707090dae75b74b79
Kamille <kamillecdocarmo@gmail.com>
2025-11-25 10:54:41 -0300

    edição métricas BD

--- Aulas_Django/BFDFlix/filme/models.py
+++ Aulas_Django/BFDFlix/filme/models.py
@@ -4,20 +4,23 @@ from django.utils import timezone
 # Create your models here.
 
 LISTA_CATEGORIAS = (
-    ("aulas", "Aulas"),
-    ("provas", "Provas"),
-    ("ciclos", "Ciclos"),
-    ("outros", "Outros"),
+    ("AULAS", "Aulas"),
+    ("PROGRAMACAO", "Programação"),
+    ("CURSOS", "Cursos"),
+    ("PROVAS", "Provas"),
+    ("CICLOS", "Ciclos"),
+    ("OUTROS", "Outros"),
 )
 
 #Criar o filme
 class Filme(models.Model):
     titulo = models.CharField(max_length=100)
     thumb = models.ImageField(upload_to='thumb_filmes')
-    descricao = models.TextField(max_length=1000)
-    categoria = models.CharField(max_length=15, choices=LISTA_CATEGORIAS)
+    descricao = models.CharField(max_length=1000)
+    categoria = models.CharField(max_length=20, choices=LISTA_CATEGORIAS)
     visualizacoes = models.IntegerField(default=0)
-    data_cricao = models.DateTimeField(default=timezone.now)
+    lancamento = models.DateTimeField(default=timezone.now)
+    # data_cricao = models.DateTimeField(default=timezone.now)
      
     def __str__ (self):
         return self.titulo
